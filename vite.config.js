66ffc66b52f0db1e022a9eb088893f3c8fbd9f58
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-06-22 16:01:25 +0530

    target changed

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        // target: "http://localhost:8000",
+        target: "https://mern-auth-all-server.netlify.app",
         changeOrigin: true,
       },
     },
